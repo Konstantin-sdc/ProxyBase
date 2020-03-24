7faa1911c1a813cfd6b1f049cfe42f34426aa792
Константин Бутенин <konstantinsdc@gmail.com>
2020-03-24 22:01:16 +0300

    Творческие метания в ProxyBaseContext. Возврат к исходному состоянию файла.

--- ProxyBaseMVC/Data/ProxyBaseContext.cs
+++ ProxyBaseMVC/Data/ProxyBaseContext.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace ProxyBaseMVC.Data {
+    
+    public class QWER { }
 
     public class ProxyBaseContext : DbContext {
 
         public ProxyBaseContext(DbContextOptions<ProxyBaseContext> options) : base(options) { }
-
 
     }
 
