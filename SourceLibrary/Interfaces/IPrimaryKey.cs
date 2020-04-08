25e669fd10eaa79860149cc98d5d17140e741994
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-08 18:53:48 +0300

    SourceLibrary.IPrimaryKey added.

---
+++ SourceLibrary/Interfaces/IPrimaryKey.cs
@@ -0,0 +1,11 @@
+﻿namespace SourceLibrary
+{
+    /// <summary>Primary key interface.</summary>
+    internal interface IPrimaryKey
+    {
+        /// <summary>Primary key.</summary>
+        int Id { get; set; }
+
+    }
+
+}
