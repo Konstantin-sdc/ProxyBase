49e38e1f9759cea5e28d3e00e87489b57f558595
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-09 16:17:45 +0300

    SourceLibrary.IGeoLocation added.

---
+++ SourceLibrary/Interfaces/IGeoLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace SourceLibrary {
+
+    /// <summary>Geographical coordinates.</summary>
+    internal interface IGeoLocation {
+
+        /// <summary>Object latitude.</summary>
+        double Latitude { get; set; }
+
+        /// <summary>Object longtitude.</summary>
+        double Longtitude { get; set; }
+
+    }
+
+}
