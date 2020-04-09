a33a4c567fa6d65ac7f59bc2649facdc59ce0894
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-09 19:16:04 +0300

    SourceLibrary.GeoLocation added.

---
+++ SourceLibrary/Classes/GeoLocation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SourceLibrary {
+
+    /// <summary>Geographical coordinates.</summary>
+    public class GeoLocation : IGeoLocation, IPrimaryKey {
+
+        public int Id { get; set; }
+
+        public double Latitude {
+            get => Latitude;
+            set {
+                if (value < -90 || value > 90) throw new ArgumentOutOfRangeException(value.ToString());
+            }
+        }
+
+        public double Longtitude {
+            get => Longtitude;
+            set {
+                if (value < -180 || value > 180) throw new ArgumentOutOfRangeException(value.ToString());
+            }
+        }
+
+        /// <summary>Return connected string <see cref="Latitude"/>,<see cref="Longtitude"/></summary>
+        public override string ToString() {
+            Latitude.ToString();
+            return $"{Latitude},{Longtitude}";
+        }
+
+    }
+
+}
