df12777c36b54a4ccc35a55aca6c0b97d8228a02
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-10 17:41:28 +0300

    SourceLibrary.ICity added.

---
+++ SourceLibrary/Interfaces/ICity.cs
@@ -0,0 +1,14 @@
+﻿namespace SourceLibrary {
+
+    /// <summary>City interface.</summary>
+    public interface ICity {
+
+        /// <summary>City title.</summary>
+        string Title { get; set; }
+
+        /// <summary>City Country.</summary>
+        ICountry Country { get; set; }
+
+    }
+
+}
