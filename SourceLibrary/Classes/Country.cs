1b2433640b5953e26961b2cf59b90aa48bf82bd8
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-10 16:57:29 +0300

    SourceLibrary.Country added.

---
+++ SourceLibrary/Classes/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SourceLibrary {
+
+    /// <summary>Country class.</summary>
+    public class Country : ICountry {
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+    }
+
+}
