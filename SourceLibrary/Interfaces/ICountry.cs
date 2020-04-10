ed489a3f9492df3cd5da0d3eb31636041b5a9466
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-10 16:18:21 +0300

    SourceLibrary.ICountry added.

---
+++ SourceLibrary/Interfaces/ICountry.cs
@@ -0,0 +1,11 @@
+﻿namespace SourceLibrary {
+
+    /// <summary>Country.</summary>
+    public interface ICountry {
+
+        /// <summary>Country title.</summary>
+        string Title { get; set; }
+
+    }
+
+}
