55c27469da2c447a9732e74a52f89e2eb6d16a5f
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-10 13:04:12 +0300

    SourceLibrary.IPAddress added.

---
+++ SourceLibrary/Classes/IPAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SourceLibrary {
+
+    /// <summary>IP-address class.</summary>
+    public class IPAddress : IIPaddress, IPrimaryKey {
+
+        public int Id { get; set; }
+
+        /// <summary>IPv4 address.</summary>
+        [MinLength(7)]
+        [MaxLength(15)]
+        public string IPString {
+            get => IPString;
+            set {
+                if (value.Length < 7 || value.Length > 15) throw new ArgumentOutOfRangeException(value);
+            }
+        }
+
+        public IGeoLocation Location { get; set; }
+
+    }
+
+}
